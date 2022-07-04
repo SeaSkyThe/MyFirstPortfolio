17e9d74bf6ffe93c8d7c5488c20fc4a2fc952e1f
SeaSkyThe <marceloer2011@gmail.com>
2022-07-04 15:38:18 -0300

    Contact form done

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        <form method='POST' action="https://getform.io/f/b6451395-14ed-41e4-87df-740826a0b518" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Contact</p>
+                <p className='text-gray-300 py-4'>- Submit the form below or send me an email at: marceloer2011@gmail.com</p>
+            </div>
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name'/>
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email'/>
+            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex item-center' >Let's Talk</button>
+        </form>
+    </div>
+  )
+}
+
+export default Contact;
